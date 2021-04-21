156b93e341fefaaf743eec05f55c60d2554c9b60
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-21 17:04:04 +0530

    Added:To validate the non-mandatory part of email

--- EmailValidation.java
+++ EmailValidation.java
@@ -4,7 +4,7 @@ public class EmailValidation {
 
   public static boolean isvalid(String emailId) {
 
-      String pattern="^[A-Z a-z 0-9]+@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}$";
+      String pattern="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}$";
 
   	Pattern P=Pattern.compile(pattern);
   	 if(emailId == null) {
